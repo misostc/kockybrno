429d099064f5f4c0bfcb8bbce5fed019fc9cf129
Michal Bábel <miso@outlook.sk>
2015-02-04 11:44:45 +0100

    Pridany lightbox pre fotogalerie

--- app/assets/javascripts/photogalleries.js
+++ app/assets/javascripts/photogalleries.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+//= require swipebox
+
+$(document).ready(function(){
+    $(".gallery a").each(function(){
+        var t = $(this);
+        t.attr("href", t.data("imgurl"));
+    }).swipebox();
+});
