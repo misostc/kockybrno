5978bd05143559d6309bf975c20e343a409a047d
Michal Bábel <michal.babel@outlook.com>
2016-01-08 20:15:36 +0100

    Cookie law fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,11 @@ $(document).ready(function(){
     });
     $("a.swipebox").swipebox();
 });
+
+$(document).on('page:change', function() {
+  if (window._gaq != null) {
+    return _gaq.push(['_trackPageview']);
+  } else if (window.pageTracker != null) {
+    return pageTracker._trackPageview();
+  }
+});
